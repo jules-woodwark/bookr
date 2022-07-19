f29489d1ad422c827b7c70fedb78732e530e02b3
Jules Woodwark <juleswoodwark@YJ1QN34GMV.local>
2022-07-19 10:03:24 +0100

    add rtd config

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDkfEyAJ5m2dRz8gnzF0A0r_RmvSwZ7yec',
@@ -9,6 +10,8 @@ const firebaseConfig = {
   storageBucket: 'bookr-5ebad.appspot.com',
   messagingSenderId: '861495615935',
   appId: '1:861495615935:web:bc8a760cc480b69c53c10a',
+  databaseURL:
+    'https://bookr-5ebad-default-rtdb.europe-west1.firebasedatabase.app/',
 };
 
 //  init firebase app
@@ -21,4 +24,7 @@ const auth = getAuth(app);
 //  collection ref
 const colRef = collection(db, 'users');
 
-export { db, auth, colRef };
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+
+export { db, auth, colRef, database };
