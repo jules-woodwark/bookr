615df7704d321aa6f6623b230863076acd794cdb
Jules Woodwark <juleswoodwark@YJ1QN34GMV.local>
2022-07-20 12:14:00 +0100

    add header/footer/regex

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -1,7 +1,20 @@
+import { styled } from '@mui/material/styles';
+import AndLogo from 'components/ui/AndLogo';
+
+const StyledFooter = styled('footer')`
+  background-color: #d82036;
+  padding: 0.5rem;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  display: flex;
+`;
+
 export default function Footer() {
   return (
-    <footer>
-      <h1>Footer</h1>
-    </footer>
+    <StyledFooter>
+      <AndLogo reversecolours="true" />
+    </StyledFooter>
   );
 }
