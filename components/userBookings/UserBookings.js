e709d5fa0dfd57f9d58ac528a336f1711257df0b
Jules Woodwark <juleswoodwark@YJ1QN34GMV.local>
2022-07-21 15:33:23 +0100

    add styling

--- components/userBookings/UserBookings.js
+++ components/userBookings/UserBookings.js
@@ -1,12 +1,25 @@
-import CardWrapper from 'components/ui/CardWrapper';
-import UserBookingsTitle from './UserBookingsTitle';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
 import UserBookingsTable from './UserBookingsTable';
+import UserBookingsTitle from './UserBookingsTitle';
+
+const StyledCard = styled(Card)`
+  padding: 2rem 3rem;
+  border-radius: 15px;
+  box-sizing: border-box;
+  overflow: scroll;
+  max-height: 600px;
+
+  ${(props) => props.theme.breakpoints.up('laptopM')} {
+    max-height: 472px;
+  }
+`;
 
 export default function UserBookings() {
   return (
-    <CardWrapper>
+    <StyledCard>
       <UserBookingsTitle />
       <UserBookingsTable />
-    </CardWrapper>
+    </StyledCard>
   );
 }
