bd22cb13c88267b18abdb0a359b86def242fcd3c
Jules Woodwark <juleswoodwark@YJ1QN34GMV.local>
2022-07-21 20:05:28 +0100

    styling club

--- components/deskBookings/DatePicker.js
+++ components/deskBookings/DatePicker.js
@@ -1,9 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import TextField from '@mui/material/TextField';
 import moment from 'moment';
+
+function convertDateToString(date) {
+  const yyyy = date.getFullYear();
+  let mm = date.getMonth() + 1; // Months are zero-indexed
+  mm = String(mm).padStart(2, '0');
+  let dd = date.getDate();
+  dd = String(dd).padStart(2, '0');
+
+  return `${yyyy}-${mm}-${dd}`;
+}
 
 export default function DatePicker({ handleDatePick }) {
   const [value, setValue] = useState(today);
@@ -11,9 +21,13 @@ export default function DatePicker({ handleDatePick }) {
   // so that the initial value is treated the same as all subsequent onChange events
   // Using 'onLoad' would trigger on every re-render instead of just the first
 
+  useEffect(() => handleDatePick({ target: { value: todayString } }), []);
+
   const today = new Date();
   const nextMonth = new Date();
   nextMonth.setMonth(today.getMonth() + 1);
+
+  const todayString = convertDateToString(today);
 
   return (
     <LocalizationProvider dateAdapter={AdapterMoment}>
