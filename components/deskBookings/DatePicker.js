ac7054025af3bd91a5cf5fcf4da45ccb10ee6f5c
Kieran Doyle <kieran.doyle@and.digital>
2022-07-21 14:46:38 +0100

    added min/max values to datepicker

--- components/deskBookings/DatePicker.js
+++ components/deskBookings/DatePicker.js
@@ -1,12 +1,10 @@
 import { useEffect } from 'react';
 
-function getToday() {
-  const today = new Date();
-
-  const yyyy = today.getFullYear();
-  let mm = today.getMonth() + 1; // Months start at 0!
+function convertDateToString(date) {
+  const yyyy = date.getFullYear();
+  let mm = date.getMonth() + 1; // Months are zero-indexed
   mm = String(mm).padStart(2, '0');
-  let dd = today.getDate();
+  let dd = date.getDate();
   dd = String(dd).padStart(2, '0');
 
   return `${yyyy}-${mm}-${dd}`;
@@ -16,9 +14,22 @@ export default function DatePicker({ handleDatePick }) {
   // Sends a manual event trigger to date picker
   // so that the initial value is treated the same as all subsequent onChange events
   // Using 'onLoad' would trigger on every re-render instead of just the first
-  useEffect(() => handleDatePick({ target: { value: getToday() } }), []);
+  useEffect(() => handleDatePick({ target: { value: todayString } }), []);
+
+  const today = new Date();
+  const nextMonth = new Date();
+  nextMonth.setMonth(today.getMonth() + 1);
+
+  const todayString = convertDateToString(today);
+  const nextMonthString = convertDateToString(nextMonth);
 
   return (
-    <input type="date" defaultValue={getToday()} onChange={handleDatePick} />
+    <input
+      type="date"
+      min={todayString}
+      defaultValue={todayString}
+      max={nextMonthString}
+      onChange={handleDatePick}
+    />
   );
 }
