d5583597f8993316ae8e0bf4c4fc29eaaec93b87
Jules Woodwark <juleswoodwark@YJ1QN34GMV.local>
2022-07-21 11:35:38 +0100

    pre rebase

--- components/deskBookings/DeskBooking.js
+++ components/deskBookings/DeskBooking.js
@@ -1,8 +1,14 @@
 import { useState, useCallback } from 'react';
+import { styled } from '@mui/material/styles';
 import DeskBookingTitle from './DeskBookingTitle';
 import DatePicker from './DatePicker';
 import DeskList from './DeskList';
 import CardWrapper from 'components/ui/CardWrapper';
+import DeskLayoutButton from './DeskLayoutButton';
+
+const StyledDiv = styled('div')`
+  display: flex;
+`;
 
 export default function DeskBooking() {
   const [selectedDate, setDate] = useState(0);
@@ -15,7 +21,10 @@ export default function DeskBooking() {
 
   return (
     <CardWrapper>
-      <DeskBookingTitle />
+      <StyledDiv>
+        <DeskBookingTitle />
+        <DeskLayoutButton />
+      </StyledDiv>
       <DatePicker handleDatePick={handleDatePick} />
       <DeskList selectedDate={selectedDate} />
     </CardWrapper>
