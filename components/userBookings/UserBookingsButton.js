a602a774e7bffadf78ec312795cd220d2138dbb4
Kieran Doyle <kieran.doyle@and.digital>
2022-07-21 11:32:18 +0100

    can unbook from userBookings view

--- components/userBookings/UserBookingsButton.js
+++ components/userBookings/UserBookingsButton.js
@@ -1,5 +1,14 @@
 import Button from '@mui/material/Button';
+import useFirebase from 'hooks/useFirebase';
+import { useCallback } from 'react';
 
-export default function UserBookingsButton() {
-  return <Button>Unbook</Button>;
+export default function UserBookingsButton({ userID, deskID, date }) {
+  const { unbookDesk } = useFirebase();
+  const club = 'grace';
+
+  const handleClick = useCallback(() => {
+    unbookDesk(userID, date, club, deskID);
+  }, []);
+
+  return <Button onClick={handleClick}>Unbook</Button>;
 }
